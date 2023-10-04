a2bc02c38efee79f9cab461bd7f4b4d278dfdf18
sorasdreams <matiasmarchessi@gmail.com>
2023-10-04 02:42:21 -0300

    Agrego modelo faltante

---
+++ src/main/java/sorasdreams/apiclima/model/SearchResult.java
@@ -0,0 +1,14 @@
+package sorasdreams.apiclima.model;
+
+public class SearchResult {
+
+    private SearchResponse[] results;
+
+    public SearchResponse[] getResults() {
+        return results;
+    }
+
+    public void setResults(SearchResponse[] results) {
+        this.results = results;
+    }
+}
