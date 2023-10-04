df4536d5babd54087e3c1ad7fce312b491054f07
sorasdreams <matiasmarchessi@gmail.com>
2023-10-04 02:40:43 -0300

    remuevo test sin uso, para evitar error al inicializar la app, ver despues

--- src/test/java/sorasdreams/apiclima/ApiClimaApplicationTests.java
+++ src/test/java/sorasdreams/apiclima/ApiClimaApplicationTests.java
@@ -1,13 +1,9 @@
 package sorasdreams.apiclima;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiClimaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
