5b7cdbbc284bfadab88fde84cb63fb3ab8ad4cff
sorasdreams <matiasmarchessi@gmail.com>
2023-10-04 02:32:41 -0300

    Agrego sobrecarga del metodo para poder realizar post request con su respectivo body

--- src/main/java/sorasdreams/apiclima/client/HttpClient.java
+++ src/main/java/sorasdreams/apiclima/client/HttpClient.java
@@ -1,10 +1,13 @@
 package sorasdreams.apiclima.client;
 
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.IOException;
 
 public interface HttpClient {
+
+    Response doRequest(String url, RequestBody requestBody) throws IOException;
 
     Response doRequest(String url) throws IOException;
 }
--- src/main/java/sorasdreams/apiclima/client/impl/OkHttpClientImpl.java
+++ src/main/java/sorasdreams/apiclima/client/impl/OkHttpClientImpl.java
@@ -1,10 +1,7 @@
 package sorasdreams.apiclima.client.impl;
 
 
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import sorasdreams.apiclima.client.HttpClient;
 
 import java.io.IOException;
@@ -20,10 +17,23 @@ public class OkHttpClientImpl implements HttpClient {
 
     /**
      * Ejecuta un request
-     * @param url
-     * @return
-     * @throws IOException
+     * @param url to use for the request
+     * @param requestBody post request body
+     * @return request response
+     * @throws IOException thrown if an error occur during the execution of this request
      */
+    @Override
+    public Response doRequest(String url, RequestBody requestBody) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        Call call = okHttpClient.newCall(request);
+
+        return call.execute();
+    }
+
     @Override
     public Response doRequest(String url) throws IOException {
         Request request = new Request.Builder()
